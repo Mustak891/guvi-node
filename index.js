187aaa5044945bffe555f01cfa9c339d3926b221
FIRST_NAME LAST_NAME <mustakahammed891@gmail.com>
2022-12-23 13:11:07 +0530

    new guvi file

---
+++ index.js
@@ -0,0 +1,42 @@
+import express, { urlencoded } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+// import { usersRouter } from './routes/users.js';
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const ConnectionDB = async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        mongoose.connect(process.env.MongoDB_url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log('MongoDB connected')
+    } catch (err) {
+        console.log(err)
+    }
+}
+await ConnectionDB();
+
+app.use(cors({origin: 'https://guvi-jobtask.netlify.app', credentials: true,}));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+// app.use('/api', usersRouter);
+
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`)});
